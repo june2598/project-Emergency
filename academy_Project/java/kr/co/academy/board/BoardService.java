a7bcefa5db3a91a32c84c2adc396bef97a6a8036
bsh <bamb00@naver.com>
2020-10-07 16:43:37 +0900

    20201007 보드 작업중 (1)

---
+++ academy_Project/java/kr/co/academy/board/BoardService.java
@@ -0,0 +1,35 @@
+package kr.co.academy.board;
+
+import java.util.List;
+
+public interface BoardService {
+	
+	//전체목록
+	public List<BoardDTO> list();
+
+	public int updateReadCnt(int bno);	//조회수 증가
+	public BoardDTO readOne(int bno,int memId);	//글 읽기
+	
+	//글쓰기 
+	public int register(BoardDTO boardDTO);
+	
+	//글삭제
+	public int delete(int bno);
+	
+	//글 수정
+	public int update(BoardDTO boardDTO);
+	
+	//댓글 쓰기를 위한 메소드
+//	public int reply
+	
+	//게시물 번호에 해당하는 댓글 번호를 가져오는 메소드
+	
+	
+	//댓글수정
+	
+	//댓글 수정 보기
+	
+	//댓글삭제 
+	
+	//파일 업로드 
+}
---
+++ academy_Project/src/main/webapp/WEB-INF/views/boardview/update.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="true" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath }" />
+<html>
+<head>
+<title>Home</title></head><body>
+<%@include file="../include/header.jsp"%>
+
+
+
+<%@include file="../include/footer.jsp"%>
+
+</body>
+</html>
